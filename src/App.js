b767b20624e358c56d1f0eb03cbef4d9ac0d94ab
KevenSoares22 <keven.v.soares@gmail.com>
2023-01-29 23:19:46 -0300

    Primeiras Configurações

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+
 import './App.css';
 
 function App() {
+
+
+const url = "http://localhost:3000/products"
+const [products, setProducts] = useState([])
+
+
+//Recebe o objeto json e converte para implementação no codigo
+useEffect(()=>{
+    async function addProducts(url){
+      const res = await fetch(url)
+      const json = await res.json()
+      console.log(json)
+    }
+
+    addProducts(url)
+    
+}, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {products.map(
+          (product)=>(<li key={product.id}>{product.name}</li>)
+        )}          
+
+      </ul>
     </div>
   );
 }
 
 export default App;
+
